2dd734a836fcdb285bc72d6bca129f87b00a47ba
demarcogleason <demarcogleason@gmail.com>
2017-06-15 21:00:01 -0500

    add users

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import Users from '../sampleUser.json';
 
-function App(props) {
-  return (
+class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    users: Users.results,
+  };
+}
+
+  render () {
+  return  (
     <div>
       <h1 className="app-header">Hello DeMarco</h1>
     </div>
   );
+}
 }
 
 export default App;
