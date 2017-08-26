c35327941465e30f5af7029a1e8d9e4ac1e6fc69
Zac Collier <zacacollier@gmail.com>
2017-08-26 10:06:38 -0500

    withState

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withState } from 'recompose';
 import axios from 'axios';
 import Header from './Header';
+import randomUsers from '../randomUsers';
 import List from './List';
 import './App.css';
 
@@ -8,52 +10,32 @@ const URL = (results = 25) =>
   `https://randomuser.me/api?results=${results}`;
 
 
-class App extends Component {
+const { users: dummyUsers } = randomUsers;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-    };
-  }
-
-  getUsers (number) {
-
-    return axios.get(URL(number))
-      .then(res => {
-        return this.setState({
-          users: res.data.results,
-        });
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-  }
-
-  componentWillMount() {
-
-    return this.getUsers(28);
-
-  }
-
-  render() {
-
-    return (
-
-      <div className="App">
-        <Header
-          contacts={this.state.users}
-        />
-        <List
-          contacts={this.state.users}
-        />
-      </div>
-
-    );
-
-  }
-
-}
+const App = withState('contacts', 'fetchContacts', { users: [] })(
+  ({ contacts: { users }, fetchContacts }) =>
+    <div className="App">
+      <Header
+        contacts={users.length ? users : dummyUsers}
+      />
+      <button
+        onClick={() =>
+          axios.get(URL(parseInt(Math.random() * 100, 10)))
+          .then(res =>
+            fetchContacts(({ users }) => ({
+              users: res.data.results,
+            })
+            )
+          )
+          .catch(err => console.error(err))
+        }
+      >
+        Fetch Contacts
+      </button>
+      <List
+        contacts={users.length ? users : dummyUsers}
+      />
+    </div>
+);
 
 export default App;
